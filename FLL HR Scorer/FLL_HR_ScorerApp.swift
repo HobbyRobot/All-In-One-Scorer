68e62c80d853a491cca4a682976eb7cb50c6d2f8
Matejvvo <77694508+Matejvvo@users.noreply.github.com>
2022-08-12 20:09:40 +0200

    Initial Commit

---
+++ FLL HR Scorer/FLL_HR_ScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLL_HR_ScorerApp.swift
+//  FLL HR Scorer
+//
+//  Created by Matej Volkmer on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FLL_HR_ScorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
